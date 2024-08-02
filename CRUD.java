4ce8a20ab64d8abc06c7f732aaac185533bb6562
susy <susycdutra@gmail.com>
2024-08-02 16:35:06 -0300

    funções básicas de inserção

--- CRUD.java
+++ CRUD.java
@@ -2,6 +2,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class CRUD {
 	
@@ -14,21 +15,12 @@ public class CRUD {
 			System.out.println("A conexão com o banco de dados foi mal sucedida.");
 			System.exit(0);
 		}
-    	
-    	System.out.println("Escolha qual tabela deseja operar:\n1 - Tabela Aluno\n2 - Tabela Nota\n");
-    	
-    	String sql = "INSERT INTO nota (nota) VALUES (?)";
+ 
+        System.out.println("Escolha qual tabela deseja operar:\n1 - Tabela Aluno\n2 - Tabela Nota\n");
 
-        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pstmt.setFloat(1, (float) 9.8);
-
-            int rowsInserted = pstmt.executeUpdate();
-            if (rowsInserted > 0) {
-                System.out.println("Uma nova nota foi inserido com sucesso!");
-            }
-        } catch (SQLException e) {
-            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
-        }
+        createAluno(conn, "Aluno");
+     
+        createNota(conn, (float) 10.0);
     }
 
     private static Connection setConnection () throws SQLException {
@@ -52,4 +44,35 @@ public class CRUD {
 
         return conn;
     }
+    
+    public static void createAluno(Connection conn, String nome) {
+    	String sql = "INSERT INTO aluno (nome) VALUES (?)";
+
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, nome);
+
+            int rowsInserted = pstmt.executeUpdate();
+            if (rowsInserted > 0) {
+                System.out.println("Um novo aluno foi inserido com sucesso!");
+            }
+        } catch (SQLException e) {
+            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+        }
+    }
+
+    public static void createNota(Connection conn, float nota) {
+    	String sql = "INSERT INTO nota (nota) VALUES (?)";
+
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setFloat(1, nota);
+
+            int rowsInserted = pstmt.executeUpdate();
+            if (rowsInserted > 0) {
+                System.out.println("Uma nova nota foi inserida com sucesso!");
+            }
+        } catch (SQLException e) {
+            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+        }
+    }
+
 }
